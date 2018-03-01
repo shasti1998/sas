96a572c179c6a8bfcc6ce3846d67713f150270a7
shasti1998 <35728443+shasti1998@users.noreply.github.com>
2018-03-01 15:57:37 +0530

    Create nivi.c

---
+++ nivi.c
@@ -0,0 +1,22 @@
+#include<studio.h>
+void main();
+{
+int array[50],max,i,n;
+{
+printf("\n enter a array element : \n");
+scanf("%d",&array);
+{
+for(i=0;i<=n;i++)
+{
+scanf("%d",array[i]);
+}
+if(max=array[0])
+{
+array[i]++;
+}
+else
+{
+printf("\n print the max element:");
+}
+getch();
+}
