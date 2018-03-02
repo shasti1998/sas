846dde084f1b6a20c3bb62808d39b05d6b2941aa
shasti1998 <35728443+shasti1998@users.noreply.github.com>
2018-03-02 14:21:12 +0530

    Create sawdef.c

---
+++ sawdef.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int min,array[i],i,n;
+printf("enter the no.of elements");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+min=array[0];
+scanf("%d",&array[i]);
+if(min>array[i])
+{
+min=array[i];
+i++;
+}
+printf("min element is %d",min);
+}
+ getch();
+ }
+
